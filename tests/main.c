8d522abd29033b322bf8a8712235048dfb8f3357
Joel Abreu <joelabreurojas@gmail.com>
2023-09-29 20:12:01 -0400

    test: change the position of the string case
    The previous position prevented see how get_string continuing to return
    the changing value of the shared global pointer. Although the previous
    behaviour is fixed, it is repositioned for testing.

--- tests/main.c
+++ tests/main.c
@@ -5,20 +5,20 @@
 int main(void)
 {
     char c = get_char("Char: ");
+    char *s = get_string("String: ");
     double d = get_double("Double: ");
     float f = get_float("Float: ");
     int i = get_int("Int: ");
     long l = get_long("Long: ");
-    char *s = get_string("String: ");
 
     putchar('\n');
 
     printf("c --> %c\n", c);
+    printf("s --> %s\n", s);
     printf("d --> %lg\n", d);
     printf("f --> %g\n", f);
     printf("i --> %i\n", i);
     printf("l --> %li\n", l);
-    printf("s --> %s\n", s);
 
     return 0;
 }
